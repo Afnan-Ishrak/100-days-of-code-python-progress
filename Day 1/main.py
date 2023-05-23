08df6bfe0c41aabd4518cfe18450b343e0aabc13
Afnan-Ishrak <134323216+Afnan-Ishrak@users.noreply.github.com>
2023-05-23 20:04:06 +0600

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,8 @@
+print("Welcome to National Football Team Name Generator")
+print("*"*30)
+
+city_name = input("What is the name of the city you grew up in?\n")
+
+animal_name = input("What is the name of the national animal there?\n")
+
+print("Your FC name is "+city_name+" "+animal_name)
